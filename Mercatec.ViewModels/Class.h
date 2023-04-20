a392cc948972bfe85309707e5b4801e380f9c1d2
Denis West <DenisWest@outlook.com>
2023-04-20 13:16:39 -0600

    feat: Creation of projects for the business

---
+++ Mercatec.ViewModels/Class.h
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::Mercatec_ViewModels::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::Mercatec_ViewModels::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
